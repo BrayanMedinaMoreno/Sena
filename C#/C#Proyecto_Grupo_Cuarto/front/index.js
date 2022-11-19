22f36e6508b99ddacf7c7c93859a7c394969e634
BrayanMedinaMoreno <Brayan0519@yahoo.com>
2022-11-19 17:08:17 -0500

    styles

--- C#/C#Proyecto_Grupo_Cuarto/front/index.js
+++ C#/C#Proyecto_Grupo_Cuarto/front/index.js
@@ -1,14 +1,27 @@
-var table = document.getElementById('table');
-var fillinTable = function (props) {
-    props.map(function (user) {
-        table.innerHTML += "<tr><td>".concat(user.ID_Booster, "</td></tr>");
-    });
+const table = document.getElementById("table");
+
+const fillinTable = (props) => {
+  props.map((user) => {
+    table.innerHTML += `<tr class='row-1'><td class='campo'>${user.ID_Booster}</td>
+    <td class='campo'>${user.Usuario_Booster}</td>
+    <td class='campo'>${user.Discord_Booster}</td>
+    <td class='campo'>${user.Correo_Booster}</td>
+    <td class='campo'>${user.Pais_Booster}</td>
+    <td class='campo'>${user.Rango_Booster}</td>
+    <td class='campo'>${user.Servidores_Booster}</td>
+    <td class='campo'>${user.Metodo_de_pago_Booster}</td>
+    <td class='campo'>${user.Posiciones_Booster}</td>
+    <td class='campo'>${user.Tipo_de_Booster}</td>
+    <td class='campo'>${user.Fecha_de_Ingreso_Booster}</td>
+    </tr>`;
+  });
 };
-window.onload = function () {
-    fetch("http://www.boosters.somee.com/api/usuario")
-        .then(function (res) { return res.json(); })
-        .then(function (res) {
-        fillinTable(res);
-        console.log(res);
+
+window.onload = () => {
+  fetch("http://www.boosters.somee.com/api/usuario")
+    .then((res) => res.json())
+    .then((res) => {
+      fillinTable(res);
+      console.log(res);
     });
 };
